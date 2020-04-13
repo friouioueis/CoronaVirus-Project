49e528c49487808d3fa6a416065a6aab68a143d0
Your Name <you@example.com>
2020-04-13 16:48:50 +0100

    login and sign up interfaces

---
+++ app/src/main/java/com/example/coronawatch/SingupActivity.kt
@@ -0,0 +1,18 @@
+package com.example.coronawatch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_singup.*
+
+class SingupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singup)
+
+        singin_link_btn.setOnClickListener {
+            startActivity(Intent(this , LoginActivity::class.java))
+        }
+    }
+}
