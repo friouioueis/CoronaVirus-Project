fb27762ffc54712ec5cb25e81e9f0367caa7042b
HouichiYousra <gy_houichi@esi.dz>
2020-04-25 02:24:37 +0100

    fix modifier article bug

--- Articles/views.py
+++ Articles/views.py
@@ -25,6 +25,7 @@ class articleView(viewsets.ModelViewSet):
         article=self.get_object()
         article.contenuAr=request.data['contenuAr']
         article.dateAr=request.data['dateAr']
+        article.terminerAR=request.data['terminerAr']
         article.save()
         return  JsonResponse({"idArticle":article.idArticle,"contenuAr":article.contenuAr,'dateAr':article.dateAr})
 
